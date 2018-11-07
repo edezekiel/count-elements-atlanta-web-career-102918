a25f45be7908926a198573fb43cfc989c45de236
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2018-11-07 14:29:41 -0500

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,22 @@
+require 'pry'
+
 def count_elements(array)
   # code goes here
+  new_hash = Hash.new(0)
+  array.each do |element|
+    new_hash[element] += 1
+  end
+  new_hash
 end
- 
+
+# array
+# => ["tortoise",
+#  "aye-aye",
+#  "honey badger",
+#  "cobra",
+#  "aye-aye",
+#  "aye-aye",
+#  "cobra",
+#  "tortoise",
+#  "tortoise",
+#  "tortoise"]
